67a4271b3aa13e7192b3204e32cc3f0f7309250d
blatham191 <39377538+blatham191@users.noreply.github.com>
2018-06-26 11:16:09 +0100

    Update phoneData.js

--- phoneData.js
+++ phoneData.js
@@ -40,3 +40,13 @@ function getPhoneData(_spreadsheetID, _worksheet, headers, tableID){
 		
 	})
 }
+
+
+function sleep(milliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > milliseconds){
+		  break;
+		}
+	}
+}
